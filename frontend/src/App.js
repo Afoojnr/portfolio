e56a5f64c553ced11a1391e1569c24f55d3b92f3
Afoojnr <91747275+Afoojnr@users.noreply.github.com>
2023-04-04 21:33:12 +0200

    NavBar completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { About, Testimonials, Work, Header, Footer, Skills } from './containers'
 import { Navbar } from './components'
+import './App.scss'
 const App = () => {
   return (
     <div className='app'>
