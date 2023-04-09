8879511079bbcd84db687fff01f1f55892b76382
Afoojnr <91747275+Afoojnr@users.noreply.github.com>
2023-04-09 18:43:39 +0200

    testimonial done

--- frontend/src/containers/Testimonials/Testimonials.jsx
+++ frontend/src/containers/Testimonials/Testimonials.jsx
@@ -13,19 +13,22 @@ const testimonials = [
     name: "Perry",
     company: "Perry Luscious",
     image: images.css,
-    feedback: "Very good design, keep it up",
+    feedback:
+      "Afolabi is a good web developer and a he is going places Insha Allah. Very good design, keep it up",
   },
   {
     name: "Anex",
     company: "Anex Image",
     image: images.react,
-    feedback: "Afoo is great developer",
+    feedback:
+      "Afolabi is a good web developer and a he is going places Insha Allah. Very good design, keep it up Afolabi is a good web developer and a he is going places Insha Allah. Very good design, keep it up",
   },
   {
     name: "Perry",
     company: "Perry Luscious",
     image: images.html,
-    feedback: "Very good design, keep it up",
+    feedback:
+      "Afolabi is a good web developer and a he is going places Insha Allah. Very good design, keep it up Afolabi is a good web developer and a he is going places Insha Allah. Very good design, keep it up ",
   },
 ];
 const brands = [
@@ -102,37 +105,6 @@ const Testimonials = () => {
 
 export default AppWrap(
   MotionWrap(Testimonials, "app__testimonial"),
-  "testimonial",
+  "testimonials",
   "app__primarybg"
 );
-
-//   return (
-//     <>
-//       {testimonials.length && (
-//         <>
-//           <div className="app__testimonial-item app__flex">
-//             <img src={urlFor(testimonials[currentIndex].imgurl)} alt={testimonials[currentIndex].name} />
-//             <div className="app__testimonial-content">
-//               <p className="p-text">{testimonials[currentIndex].feedback}</p>
-//               <div>
-//                 <h4 className="bold-text">{testimonials[currentIndex].name}</h4>
-//                 <h5 className="p-text">{testimonials[currentIndex].company}</h5>
-//               </div>
-//             </div>
-//           </div>
-
-//           <div className="app__testimonial-btns app__flex">
-//             <div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
-//               <HiChevronLeft />
-//             </div>
-
-//             <div className="app__flex" onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
-//               <HiChevronRight />
-//             </div>
-//           </div>
-//         </>
-//       )}
-
-//     </>
-//   );
-// };
